5cf1565ea7e001d3434f68b91d743433ebfe6355
laill <lailiangliang@lihetech.com>
2018-10-19 13:49:04 +0800

    提交

--- src/java/main/Test.java
+++ src/java/main/Test.java
@@ -6,6 +6,6 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world 34 64664577653364646464");
+        System.out.println("hello world");
     }
 }
